93e0a11a988dd76d9db961da53220b39d2a0d803
장영재 <urinaner@naver.com>
2024-06-03 16:06:36 +0900

    Feat: DB_Connect, DB_Close

---
+++ src/util/DB_Close.java
@@ -0,0 +1,69 @@
+package util;
+
+import java.sql.*;
+public class DB_Close {
+    public static Connection getConnection() {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "mytest", "mytest");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static void close(PreparedStatement pstmt, Connection conn) {
+        if (pstmt != null) {
+            try {
+                if (!pstmt.isClosed())
+                    pstmt.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                pstmt = null;
+            }
+        }
+        if (conn != null) {
+            try {
+                if (!conn.isClosed())
+                    conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                conn = null;
+            }
+        }
+    }
+
+    public static void close(ResultSet rs, PreparedStatement pstmt, Connection conn) {
+        if (rs != null) {
+            try {
+                if (!rs.isClosed())
+                    rs.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                rs = null;
+            }
+        }
+        if (pstmt != null) {
+            try {
+                if (!pstmt.isClosed())
+                    pstmt.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                pstmt = null;
+            }
+        }
+        if (conn != null) {
+            try {
+                if (!conn.isClosed())
+                    conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                conn = null;
+            }
+        }
+    }
+}
--- src/util/DB_Connect.java
+++ src/util/DB_Connect.java
@@ -2,20 +2,16 @@ package util;
 import java.sql.*;
 
 public class DB_Connect {
-    public static void main(String[] args) {
-        Statement stmt;
-        Connection conn;
+    static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/TEST?serverTimezone=UTC","root","wkddudwo1!");
-            System.out.println("DB connection complete");
-
-            stmt = conn.createStatement();
-
         } catch (ClassNotFoundException e) {
-            System.out.println("JDBC drive road error");
-        } catch (SQLException e) {
-            System.out.println("DB connection error");
+            System.err.println("µå¶óÀÌ¹ö °Ë»ö ½ÇÆÐ");
+            e.printStackTrace();
         }
     }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/TEST?serverTimezone=UTC","root","wkddudwo1!");
+    }
+
 }
